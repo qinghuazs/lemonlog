4deadfdd440f066a5c1db61d2341858201e94a23
王星磊 <wangxinglei@yonyou.com>
2025-09-04 17:16:35 +0800

    group by

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -29,7 +29,7 @@ export default defineUserConfig({
       ],
       "/mysql/": [
         { text: "应用", children: ["index"] },
-        { text: "原理", children: ["datatype","index","mvcc"] },
+        { text: "原理", children: ["datatype","index","mvcc","group-by"] },
         { text: "性能优化", children: ["explain"] },
         { text: "问题处理", children: ["deadlock"] },
        
